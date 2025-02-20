98ee1f65534d8d746e329dbc25f0b28d3dce5744
sunnydodti <sunnydodti@gmail.com>
2025-02-20 07:07:56 +0530

    cache api responses

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
 
+import 'package:hive_ce/hive.dart';
 import 'package:http/http.dart' as http;
 
+import '../data/constants.dart';
+
 class ApiService {
+  static final Box _box = Hive.box(Constants.box);
   static final String _currencyListUrl =
       'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json';
   static final String _exchangeRateUrl =
@@ -32,6 +36,10 @@ class ApiService {
     try {
       DateTime now = DateTime.now().subtract(Duration(hours: 5));
       String date = now.toUtc().toIso8601String().substring(0, 10);
+
+      Map<dynamic, dynamic>? cache = _getExchangeRateFromCache(code, date);
+      if (cache != null && cache.isNotEmpty) return cache;
+
       String url = _exchangeRateUrl.replaceFirst('__code__', code);
       url = url.replaceFirst('__date__', date);
 
@@ -43,6 +51,7 @@ class ApiService {
         return exchangeRate;
       }
       exchangeRate = data[code];
+      _saveExchangeRateToCache(code, date, exchangeRate);
       // for (var key in data[code].keys) {
       //   exchangeRate.add({"code": key, "rate": data[code][key]});
       // }
@@ -50,5 +59,14 @@ class ApiService {
       print(e);
     }
     return exchangeRate;
+  }
+
+  static _getExchangeRateFromCache(String code, String date) {
+    return _box.get("cache_${date}_$code", defaultValue: {});
+  }
+
+  static _saveExchangeRateToCache(
+      String code, String date, Map<dynamic, dynamic> data) {
+    _box.put("cache_${date}_$code", data);
   }
 }
