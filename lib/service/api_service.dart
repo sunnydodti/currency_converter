0b94bc5d048dc4e8805d9f20647e09718b246c72
sunnydodti <sunnydodti@gmail.com>
2025-02-24 15:01:24 +0530

    add secondary api endpoint

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -7,23 +7,36 @@ import '../data/constants.dart';
 
 class ApiService {
   static final Box _box = Hive.box(Constants.box);
-  static final String _currencyListUrl =
-      'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json';
-  static final String _exchangeRateUrl =
-      'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@__date__/v1/currencies/__code__.json';
+
+  static final String _currencyListUrlPrimary =
+      'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.min.json';
+  static final String _exchangeRateUrlPrimary =
+      'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/__code__.min.json';
+
+  static final String _currencyListUrlSecondary =
+      'https://latest.currency-api.pages.dev/v1/currencies.min.json';
+  static final String _exchangeRateUrlSecondary =
+      'https://latest.currency-api.pages.dev/v1/currencies/__code__.min.json';
+
+  static final Duration timeout = const Duration(seconds: 5);
 
   static Future<List<Map<String, String>>> getCurrencyList() async {
     List<Map<String, String>> currencies = [];
 
     try {
-      final response = await http.get(Uri.parse(_currencyListUrl));
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> data = json.decode(response.body);
-
-        data.forEach((key, value) {
-          currencies.add({"name": value, "code": key});
-        });
+      http.Response response =
+          await http.get(Uri.parse(_currencyListUrlPrimary)).timeout(timeout);
+      if (response.statusCode != 200) {
+        response = await http
+            .get(Uri.parse(_currencyListUrlSecondary))
+            .timeout(timeout);
+        if (response.statusCode != 200) return currencies;
       }
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      data.forEach((key, value) {
+        currencies.add({"name": value, "code": key});
+      });
     } catch (e) {
       print(e);
     }
@@ -34,19 +47,26 @@ class ApiService {
     Map<dynamic, dynamic> exchangeRate = {};
 
     try {
-      DateTime now = DateTime.now().subtract(Duration(hours: 5));
-      String date = now.toUtc().toIso8601String().substring(0, 10);
+      DateTime now = DateTime.now().toUtc();
+      String date = now.toIso8601String().substring(0, 10);
 
       Map<dynamic, dynamic>? cache = _getExchangeRateFromCache(code, date);
       if (cache != null && cache.isNotEmpty) return cache;
 
-      String url = _exchangeRateUrl.replaceFirst('__code__', code);
-      url = url.replaceFirst('__date__', date);
+      String url = _exchangeRateUrlPrimary.replaceFirst('__code__', code);
+      // url = url.replaceFirst('latest', date);
 
-      final response = await http.get(Uri.parse(url));
+      http.Response response = await http.get(Uri.parse(url)).timeout(timeout);
+      if (response.statusCode != 200) {
+        response = await http
+            .get(Uri.parse(_exchangeRateUrlSecondary))
+            .timeout(timeout);
+        if (response.statusCode != 200) return exchangeRate;
+      }
 
-      if (response.statusCode != 200) return exchangeRate;
       Map<String, dynamic> data = json.decode(response.body);
+      // DateTime currencyDate = DateTime.parse(data['date']);
+
       if (!data.containsKey(code) || data[code].isEmpty) {
         return exchangeRate;
       }
